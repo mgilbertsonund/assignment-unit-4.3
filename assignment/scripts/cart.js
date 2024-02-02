1c3b027e9e5a3c11b96836130512266210b1e7b8
Matt Gilbertson <mgilbertsonund@gmail.com>
2024-02-01 20:46:47 -0600

    last one was tricky, still working on it

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,8 +1,51 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
 
+function addItem(item) {
+    while(isFull() === false) {
+        basket.push(item);
+        return true;
+    }
+ }
+addItem('dog');
+addItem('apples');
+addItem('cat');
+addItem('bird');
+addItem('snake');
+addItem('snow');
+console.log(`basket is now ${basket}`);
 
+function listItems() {
+    for(let variable of basket) {
+        console.log(basket);
+    }
+ }
+listItems();
+
+function empty() {
+    while (basket.length > 0)
+        basket.pop();
+}
+empty();
+console.log(basket);
+
+const maxItems = 5;
+
+function isFull() {
+    if(basket.length < 5) {
+        return false;
+    } else if(basket.length >= 5) {
+        return true;
+    }
+}
+
+// function removeItem(item) {
+//     basket.prototype.indexOf.call(item);
+// }
+
+// console.log(removeItem('bird'));
 
 
 
